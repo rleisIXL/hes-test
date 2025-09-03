36d44efdacd17e7154e222d5ce0f4068b46db5d0
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 04:34:21 +0000

    trying global changes again

--- HES_Project/Simulation_Data.py
+++ HES_Project/Simulation_Data.py
@@ -2,17 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Simulation Data", layout="wide")
 
-st.markdown(
-    """
-    <style>
-        /* Sidebar background color */
-        section[data-testid="stSidebar"] {
-            background-color: #f0f0f5;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Content
 st.markdown("<h2 style='color:#1F6B51; font-weight:bold;'>HEALTH EQUITY SIMULATION</h2>", unsafe_allow_html=True)
