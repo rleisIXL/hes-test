ec315685cfc8f67fc78c1fa0412daa5bbd7643f9
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 04:24:08 +0000

    test

--- HES_Project/pages/Initial_Evaluation.py
+++ HES_Project/pages/Initial_Evaluation.py
@@ -2,21 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Initial Evalutation", layout="wide")
 
-st.markdown(
-    """
-    <style>
-        /* Sidebar background color */
-        section[data-testid="stSidebar"] {
-            background-color: #f0f0f5;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Sidebar content
-st.sidebar.title("Sidebar")
-st.sidebar.write("This is a customized sidebar.")
-
 # Content
 st.markdown("<h2 style='color:#1F6B51; font-weight:bold;'>HEALTH EQUITY SIMULATION</h2>", unsafe_allow_html=True)
