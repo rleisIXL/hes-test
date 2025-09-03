67f4c8a7a1f4f746c1d791f74568bcaca2bd298a
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 04:02:03 +0000

    Test

---
+++ HES_Project/Simulation_Data.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Health Equity Simulation", layout="wide")
+
+st.title("🏠 Welcome to the Health Equity Simulation App")
+st.write("Use the sidebar to navigate between pages.")
