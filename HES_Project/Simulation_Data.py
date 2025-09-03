c339ccdae308987dadcf963d1590ef71b6cdade4
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 12:42:24 +0000

    Trying app page navigation structure

--- HES_Project/Simulation_Data.py
+++ HES_Project/Simulation_Data.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+#Page Navigation
+pages=[
+    st.Page(page:"app_pages/Demographics.py", title="Demographics")
+    st.Page(page:"app_pages/Initial_Evaluation.py", title="Initial Evaluation")
+]
+
+#Adding pages to the sidebar
+pg = st. navigation(paged, postition="sidebar", expanded=True)
+
+#Running the app
+pr.run()
+
 st.set_page_config(page_title="Simulation Data", layout="wide")
 
 # Content
