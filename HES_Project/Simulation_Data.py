ddd6db40b821aedd5f2f2e44aa16ca61ae72c87e
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 04:25:33 +0000

    changing color for sidebar

--- HES_Project/Simulation_Data.py
+++ HES_Project/Simulation_Data.py
@@ -3,14 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="Simulation Data", layout="wide")
 
 st.markdown(
-    """
+    
     <style>
         /* Sidebar background color */
         section[data-testid="stSidebar"] {
             background-color: #f0f0f5;
         }
     </style>
-    """,
+    
     unsafe_allow_html=True
 )
 
