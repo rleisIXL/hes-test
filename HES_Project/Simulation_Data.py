fb98aa75d52f2e40291e10a21ea55c74b760cc9b
rleisIXL <rebecca.leis@goixl.com>
2025-09-03 12:52:02 +0000

    that didnt work

--- HES_Project/Simulation_Data.py
+++ HES_Project/Simulation_Data.py
@@ -1,10 +1,4 @@
 import streamlit as st
 
-def page_link():
-    st.page_link("app_pages/Demographics.py", label="Demographics Page")
-    st.page_link("app_pages/Initial_Evaluation.py", label="Eval Page")
-
-st.set_page_config(page_title="Simulation Data", layout="wide")
-
 # Content
 st.markdown("<h2 style='color:#1F6B51; font-weight:bold;'>HEALTH EQUITY SIMULATION</h2>", unsafe_allow_html=True)
